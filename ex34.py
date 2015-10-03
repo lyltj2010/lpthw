24c4bc1d02d1078d86f01b03b66e3a3bb2b48ae1
9-win-yong <lyuyongle@gmail.com>
2015-10-03 14:13:23 -0500

    Create ex34.py

---
+++ ex34.py
@@ -0,0 +1,10 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platyus']
+print animals
+print animals[1]
+print animals[3-1]
+print animals[1-1]
+print animals[3]
+print animals[5-1]
+print animals[2]
+print animals[6-1]
+print animals[4]
